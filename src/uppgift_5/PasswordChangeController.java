2c8b10d33dc425ff29745df6848b5a0e074f5585
Fredrik Filén <fredrikfihlen@gmail.com>
2020-12-18 15:02:25 +0100

    updated changePassword

--- src/uppgift_5/PasswordChangeController.java
+++ src/uppgift_5/PasswordChangeController.java
@@ -3,6 +3,7 @@ package uppgift_5;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class PasswordChangeController {
@@ -14,9 +15,13 @@ public class PasswordChangeController {
 	private TextField newPasswordField;
 
 	@FXML
+	private Label infoLabel;
+
+	@FXML
 	void savePassword(ActionEvent event) {
 
 		MainController.getUser().setPassword(newPasswordField.getText());
+		infoLabel.setVisible(true);
 	}
 
 }
